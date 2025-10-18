11fc5ca28fa21f65bd2437e046136398c62b926b
Lt-Hariom-2002 <143435675+Lt-Hariom-2002@users.noreply.github.com>
2025-10-18 10:25:13 +0530

    Update main.tf
    Signed-off-by: Lt-Hariom-2002 <143435675+Lt-Hariom-2002@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,31 +1,24 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "tomcat_server" {
-  ami           = "ami-0360c520857e3138f"
-  instance_type = "t2.micro"
-  security_groups = ["sgalltraffic"]  # Reference the existing security group
-  key_name      = "mujahed"
-}
+
 
 resource "aws_instance" "mysql_server" {
-  ami           = "ami-0360c520857e3138f"
-  instance_type = "t2.micro"
-  security_groups = ["sgalltraffic"]  # Reference the existing security group
-  key_name      = "mujahed"
+  ami           = "ami-0341d95f75f311023"
+  instance_type = "t3.micro"
+  security_groups = ["launch-wizard-1"]  # Reference the existing security group
+  key_name      = "hariom"
 }
 
 resource "aws_instance" "maven_server" {
-  ami           = "ami-0360c520857e3138f"
-  instance_type = "t2.micro"
-  security_groups = ["sgalltraffic"]  # Reference the existing security group
-  key_name      = "mujahed"
+  ami           = "ami-0341d95f75f311023"
+  instance_type = "t3.micro"
+  security_groups = ["launch-wizard-1"]  # Reference the existing security group
+  key_name      = "hariom"
 }
 
 
-output  "tomcat_server_ip" {
-  value = aws_instance.tomcat_server.public_ip
-}
+
 
 output  "mysql_server_ip" {
   value = aws_instance.mysql_server.public_ip
